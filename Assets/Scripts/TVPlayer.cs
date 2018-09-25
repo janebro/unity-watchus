ff45185a8bb1fb25ad37ba38d15a6355c2519fc3
Ernesto Janebro <ernesto.camurca@gmail.com>
2018-09-25 17:47:49 -0300

    Presence detection for TV Screen

---
+++ Assets/Scripts/TVPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class TVPlayer : MonoBehaviour
+{
+    VideoPlayer vp;
+
+    private void Start()
+    {
+        vp = GetComponent<VideoPlayer>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        vp.Stop();
+        vp.Play();
+    }
+}
+ 
