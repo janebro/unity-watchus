dbe6f5759543c8bb8c7509e5f3efe6da617ec3a5
Ernesto Janebro <ernesto.camurca@gmail.com>
2018-09-28 17:31:39 -0300

    Fade in title screen

---
+++ Assets/Scripts/CanvasFadeIn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasFadeIn : MonoBehaviour {
+
+    bool fadeIn = false;
+    CanvasGroup canvasGroup;
+
+	// Use this for initialization
+	void Start () {
+        canvasGroup = GetComponent<CanvasGroup>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (fadeIn)
+        {
+            canvasGroup.alpha = Mathf.Lerp(canvasGroup.alpha, 1f, Time.deltaTime);
+        }
+	}
+
+    public void SetFadeIn()
+    {
+        fadeIn = true;
+    }
+}
---
+++ Assets/Scripts/IntroFadeIn.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroFadeIn : MonoBehaviour
+{
+    public Canvas eventCanvas;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            eventCanvas.GetComponent<CanvasFadeIn>().SetFadeIn();
+        }
+    }
+}
