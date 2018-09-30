7d06c9a5136812aed1044b785ef9ea912da91f09
Ernesto Janebro <ernesto.camurca@gmail.com>
2018-09-30 12:01:03 -0300

    Basic cutscene with game start

--- Assets/Scripts/IntroFadeIn.cs
+++ Assets/Scripts/IntroFadeIn.cs
@@ -1,5 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class IntroFadeIn : MonoBehaviour
 {
@@ -10,6 +12,14 @@ public class IntroFadeIn : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             eventCanvas.GetComponent<CanvasFadeIn>().SetFadeIn();
+            StartCoroutine(LoadGame());
         }
+    }
+
+    IEnumerator LoadGame()
+    {
+        yield return new WaitForSeconds(5f);
+
+        SceneManager.LoadScene(1);
     }
 }
